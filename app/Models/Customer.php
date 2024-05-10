d4ffd379de60db234e51020985c1979b6370f520
Farid Azhari <faridazhari07@gmail.com>
2024-05-11 01:13:50 +0700

    add Eloquent Accessors

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Customer extends Model
 {
@@ -21,5 +22,13 @@ class Customer extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => \Carbon\Carbon::parse($value)->translatedFormat('l, d F Y'),
+        );
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 class Product extends Model
 {
     use HasFactory;
@@ -22,4 +22,18 @@ class Product extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => url('/storage/products/' . $value),
+        );
+    }
+
+    public function reviewAvgRating(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? substr($value, 0, 3) : 0,
+        );
+    } 
 }
